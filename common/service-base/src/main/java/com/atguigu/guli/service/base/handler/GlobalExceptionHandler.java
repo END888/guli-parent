e1f32ea0f7a3e9dd8c958e2e9e7746f2229c2da6
ycy <247208124@qq.com>
2022-07-20 15:27:35 +0800

    Part16、课程类目管理完成

--- common/service-base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/com/atguigu/guli/service/base/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.atguigu.guli.service.base.handler;
 
+import com.atguigu.guli.service.base.exception.GuliException;
 import com.atguigu.guli.service.base.result.R;
 import com.atguigu.guli.service.base.result.ResultCodeEnum;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +26,13 @@ public class GlobalExceptionHandler {
     public R error(HttpMessageNotReadableException e){
         log.error(ExceptionUtils.getStackTrace(e));
         return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
+    }
+
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody
+    public R error(GuliException e){
+        log.error(ExceptionUtils.getStackTrace(e));
+        return R.error().message(e.getMessage()).code(e.getCode());
     }
 
 }
